ca17893d2a07a959b706f6b5650f14814abde7f7
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2022-10-13 09:25:25 -0500

    express

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +13,40 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+router.get('/me', (req, res) => {
+    res.send({ me: 'hello' })
+})
+
+// cats
+const routes = ['get /cat', 'get /cat/:id', 'post /cat', 'put /cat/:id', 'delete /cat/:id']
+
+router.route('/cat')
+    .get()
+    .post()
+
+router.route('/cat/:id')
+    .get()
+    .put()
+    .delete()
+
+app.use('/api', router)
+// CRUD 
+app.get('/', (req, res, next) => {
+    // res.send({ data: 1 })
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send({ data: 2 })
+})
+
+app.post('/data', (req, res) => {
+    console.log(req.body);
+    res.send({ ok: true })
+})
+
+export const start = () => {
+    app.listen(95, () => {
+        console.log('server is on port 95');
+    })
+}
