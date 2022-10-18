7945b19c23c993f42c398c087507acc295a8fde6
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2022-10-18 09:41:10 -0500

    resolving lesson-1 exercise

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@ const baseConfig = {
   env,
   isDev: env === 'development',
   isTest: env === 'testing',
-  port: 3000,
+  port: 95,
   secrets: {
     jwt: process.env.JWT_SECRET,
     jwtExp: '100d'
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,20 @@
 import { Router } from 'express'
+const controller = (req, res) => {
+    res.send({ message: 'hello' })
+}
 
 const router = Router()
+// /api/item
+router
+    .route('/')
+    .get(controller)
+    .post(controller)
 
-export default router
+// /api/item/:id
+router
+    .route('/:id')
+    .put(controller)
+    .delete(controller)
+    .get(controller)
+
+module.exports = router
