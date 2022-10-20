4327894a20db94b7e92ed2f8cb76a7e08aebc33f
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2022-10-20 17:17:42 -0500

    lesson-2 exercise

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -6,6 +6,26 @@ const itemSchema = new mongoose.Schema({
         required: true,
         trim: true,
         maxlength: 50
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['active', 'complete', 'pastdue'],
+        default: 'active'
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+        type: mongoose.SchemaTypes.ObjectID,
+        ref: 'user',
+        required: true
+    },
+    list: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true,
+        ref: 'list'
     }
 }, { timestamps: true })
+
+itemSchema.index({ list: 1, name: 1 }, { unique: true })
 export const Item = mongoose.model('item', itemSchema)
