2de70d8b5d47264f79542d58a6c37165211489dc
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2022-10-24 10:05:36 -0500

    lesson-4 exercise complete
    Signed-off-by: Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,55 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const user = req.user._id
 
-export const getMany = model => async (req, res) => {}
+  const doc = await model.findOne({ _id: id, createdBy: user }).exec()
 
-export const createOne = model => async (req, res) => {}
+  if (!doc) {
+    return res.status(404).end()
+  }
+  res.status(200).json({ data: doc })
+}
 
-export const updateOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const user = req.user._id
 
-export const removeOne = model => async (req, res) => {}
+  const docs = await model.find({ createdBy: user }).exec()
+
+  res.status(200).json({ data: docs })
+}
+
+export const createOne = model => async (req, res) => {
+  const createdBy = req.user._id
+  const body = req.body
+  const doc = await model.create({ ...body, createdBy })
+
+  res.status(201).json({ data: doc })
+}
+
+export const updateOne = model => async (req, res) => {
+  const doc = await model.findOneAndUpdate({
+    _id: req.params.id,
+    createdBy: req.user._id
+  },
+    req.body,
+    { new: true }
+  )
+  if (!doc) {
+    return res.status(400).end()
+  }
+  res.status(200).json({ data: doc })
+}
+
+export const removeOne = model => async (req, res) => {
+  const doc = await model.findOneAndRemove({
+    _id: req.params.id,
+    createdBy: req.user._id
+  }).exec()
+  if (!doc) {
+    return res.status(400).end()
+  }
+  res.status(200).json({ data: doc })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
